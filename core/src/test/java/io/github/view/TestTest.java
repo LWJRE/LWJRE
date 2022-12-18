8187e6c803bb1d2061c35260b4b620a6c80f8ada
Nico <nico.hex6@gmail.com>
2022-12-18 09:28:27 +0100

    Added test dependency

---
+++ core/src/test/java/io/github/view/TestTest.java
@@ -0,0 +1,12 @@
+package io.github.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+	@Test
+	void test() {
+		Assertions.assertTrue(true);
+	}
+}
