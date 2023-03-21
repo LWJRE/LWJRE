dafef3dd290150b2ca1f415280571d37556a2777
Nico <nico.hex6@gmail.com>
2023-03-21 13:50:47 +0100

    Added javadoc

--- core/src/main/java/gamma/engine/core/annotations/EditorDegrees.java
+++ core/src/main/java/gamma/engine/core/annotations/EditorDegrees.java
@@ -5,6 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the value of this field should be converted to degrees when shown in the inspector.
+ * The value shown in the inspector will be in degrees, the actual value of this field will be in radians.
+ * This annotation is applicable to real types and real vectors.
+ *
+ * @see Math#toRadians(double)
+ * @see Math#toDegrees(double)
+ *
+ * @author Nico
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface EditorDegrees {
--- core/src/main/java/gamma/engine/core/annotations/EditorRange.java
+++ core/src/main/java/gamma/engine/core/annotations/EditorRange.java
@@ -5,13 +5,40 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the inspector should limit the value of this field between a maximum and a minimum value.
+ * If this annotation is present, the inspector will show this field as a "draggable" number input.
+ * This annotation is applicable to integer types, real types, and vector types.
+ *
+ * @see EditorSlider
+ *
+ * @author Nico
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface EditorRange {
 
+	/**
+	 * The maximum value that can be set to this field in the inspector.
+	 * By default {@link Float#POSITIVE_INFINITY}.
+	 *
+	 * @return The maximum value that can be set to this field in the inspector
+	 */
 	float max() default Float.POSITIVE_INFINITY;
 
+	/**
+	 * The minimum value that can be set to this field in the inspector.
+	 * By default {@link Float#NEGATIVE_INFINITY}.
+	 *
+	 * @return The minimum value that can be set to this field in the inspector
+	 */
 	float min() default Float.NEGATIVE_INFINITY;
 
+	/**
+	 * The speed at which this value can be changed by dragging the mouse in the inspector.
+	 * This value represents the amount changed for every pixel.
+	 *
+	 * @return The amount by which the field's value should for every pixel in the inspector.
+	 */
 	float step() default 0.001f;
 }
